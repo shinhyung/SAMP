384fbb524ccbf0b8452e0ad564f20a3fe47a4a13
kanghyunsoo <dmhskang@naver.com>
2019-07-15 21:03:14 +0900

    api.go

--- src/api.go
+++ src/api.go
@@ -2,6 +2,50 @@ package main
 
 import "github.com/kataras/iris"
 
-func memberUnregister(ctx iris.Context) {
+func userPost(ctx iris.Context) {
+	
+}
+
+func userDelete(ctx iris.Context) {
 
 }
+func clubPut(ctx iris.Context) {
+
+}
+func clubPost(ctx iris.Context) {
+
+}
+func clubDelete(ctx iris.Context) {
+
+}
+func memberDelete(ctx iris.Context) {
+
+}
+func memberGet(ctx iris.Context) {
+
+}
+func memberGetList(ctx iris.Context) {
+
+}
+func member(ctx iris.Context) {
+
+}
+func (ctx iris.Context) {
+
+}
+func (ctx iris.Context) {
+
+}
+func (ctx iris.Context) {
+
+}
+func (ctx iris.Context) {
+
+}
+func (ctx iris.Context) {
+
+}
+func (ctx iris.Context) {
+
+}
+
